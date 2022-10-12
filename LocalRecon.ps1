cb264e9eb9854f12112635b493b4126bca12364b
AMIKA <60911813+AMIKA-pl@users.noreply.github.com>
2022-10-12 08:53:21 +0100

    Add files via upload

---
+++ LocalRecon.ps1
@@ -0,0 +1,31 @@
+<#
+STEP: LOCAL RECONNAISSANCE
+** Run PowerShell with Administrative Privileges **
+#>
+
+#T1033
+whoami;
+
+#T1082
+systeminfo;
+fsutil fsinfo drives;
+
+#T1083, T1059, T1059.003
+cmd /c dir .\\\\;
+
+#T1083
+attrib /D;
+
+#T1033
+net user;
+net accounts;
+
+#T1069.001
+net localgroup;
+
+#T1033, T1059.001
+powershell -ExecutionPolicy Bypass -File "C:\Users\ADMINI~1\AppData\Local\Temp\1\ExtractLogins.ps1";
+
+#T1564.013, T1018
+powershell -ExecutionPolicy Bypass -File "C:\Users\ADMINI~1\AppData\Local\Temp\1\DNSDump.ps1";
+
